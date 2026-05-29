5d43a34ff8ec5e2945d1feaeb5e974e46d73d28d
JCPrime01 <vanzokiller@gmail.com>
2026-05-29 18:34:10 -0300

    chore: shareCode KBU4KYFX5WN -> QM37GAS1NGI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=KBU4KYFX5WN&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=QM37GAS1NGI&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
